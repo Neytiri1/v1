5aa182e2cecda8095eea89e98fdf1de9c2ca9338
Neytiri1 <93209571+Neytiri1@users.noreply.github.com>
2021-11-10 22:57:06 +0100

    Add files via upload

---
+++ assets/js/chara.js
@@ -0,0 +1,39 @@
+			var name_ = document.getElementById("name");
+			var nickname = document.getElementById("nickname");
+			var height = document.getElementById("height");
+			var birthday = document.getElementById("birthday");
+		
+			function Image1(){
+				document.getElementById("image").src = 'perso.png';
+				document.getElementById("illustration").src = 'illustration.png';
+				name_.innerHTML = "Elysia";
+				nickname.innerHTML = "Elfe rose";
+				height.innerHTML = "163 cm/54,8 kg";
+				birthday.innerHTML = "Anniversaire :11 novembre";
+			}
+
+			function Image2(){
+				document.getElementById("image").src = 'perso2.png';
+				document.getElementById("illustration").src = 'illustration2.png';
+				name_.innerHTML = "Kiana Kaslana";
+				nickname.innerHTML = "Herrscher des flammes";
+				height.innerHTML = "163cm / 49kg";
+				birthday.innerHTML = "Anniversaire : 7 Décembre";
+			}
+
+			function Image3(){
+				document.getElementById("image").src = 'perso3.png';
+				document.getElementById("illustration").src = 'illustration3.png';
+				name_.innerHTML = "Bronya Zaychik";
+				nickname.innerHTML = "Haxxor lapine";
+				height.innerHTML = "163 cm/54,8 kg";
+				birthday.innerHTML = "Anniversaire : 18 août";
+			}
+			function Image4(){
+				document.getElementById("image").src = 'perso4.png';
+				document.getElementById("illustration").src = 'illustration4.png';
+				name_.innerHTML = "Durandal";
+				nickname.innerHTML = "Dea Anchora";
+				height.innerHTML = "165cm/54kg";
+				birthday.innerHTML = "Anniversaire : 1er janvier";
+			}
